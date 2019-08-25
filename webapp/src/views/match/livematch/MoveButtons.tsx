aaef5f5311c672543ab8c25476c3b108a8fda528
Lucas Pickering <lucas@lucaspickering.me>
2019-08-25 14:18:19 -0400

    Add move button pentagon

--- webapp/src/views/match/livematch/MoveButtons.tsx
+++ webapp/src/views/match/livematch/MoveButtons.tsx
@@ -4,11 +4,30 @@ import { Move } from 'state/match';
 import MoveIconCircle from './MoveIconCircle';
 
 const useLocalStyles = makeStyles(({ spacing }) => ({
+  root: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(5, 1fr)',
+    gridTemplateRows: 'repeat(3, 1fr)',
+    gridRowGap: 60,
+    gridTemplateAreas: `
+    '.. .. ro .. ..'
+    'li .. .. .. pa'
+    '.. sp .. sc ..'
+    `,
+  },
   button: {
     margin: spacing(1),
     padding: 0,
   },
 }));
+
+const areaMapping = {
+  [Move.Rock]: 'ro',
+  [Move.Paper]: 'pa',
+  [Move.Scissors]: 'sc',
+  [Move.Lizard]: 'li',
+  [Move.Spock]: 'sp',
+};
 
 interface Props {
   disabled: boolean;
@@ -18,19 +37,19 @@ interface Props {
 const MoveButtons = ({ disabled, onClick }: Props): React.ReactElement => {
   const localClasses = useLocalStyles();
   return (
-    <>
+    <Grid className={localClasses.root} item>
       {Object.values(Move).map((move: Move) => (
-        <Grid key={move} item>
-          <IconButton
-            className={localClasses.button}
-            disabled={disabled}
-            onClick={() => onClick(move)}
-          >
-            <MoveIconCircle move={move} />
-          </IconButton>
-        </Grid>
+        <IconButton
+          key={move}
+          className={localClasses.button}
+          disabled={disabled}
+          style={{ gridArea: areaMapping[move] }}
+          onClick={() => onClick(move)}
+        >
+          <MoveIconCircle move={move} />
+        </IconButton>
       ))}
-    </>
+    </Grid>
   );
 };
 
