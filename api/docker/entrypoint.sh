e32aa546fe4c4b96d26c207196c5f15d9e4b978f
Lucas Pickering <lucas@lucaspickering.me>
2019-08-25 23:13:48 -0400

    Fix API entrypoint

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-echo "Reading DB password from $RPS_DB_PASSWORD_FILE"
-export RPS_DB_PASSWORD=$(cat $RPS_DB_PASSWORD_FILE)
-echo "Reading secret key from $RPS_SECRET_KEY_FILE"
-export RPS_SECRET_KEY=$(cat $RPS_SECRET_KEY_FILE)
+if [ ! -z ${RPS_DB_PASSWORD_FILE} ]; then
+    echo "Reading DB password from $RPS_DB_PASSWORD_FILE"
+    export RPS_DB_PASSWORD=$(cat $RPS_DB_PASSWORD_FILE)
+fi
+if [ ! -z ${RPS_SECRET_KEY_FILE} ]; then
+    echo "Reading secret key from $RPS_SECRET_KEY_FILE"
+    export RPS_SECRET_KEY=$(cat $RPS_SECRET_KEY_FILE)
+fi
+exec "$@"
