be81d260b54f05bb70bfe52dbaf0e1d6f01fad25
Lucas Pickering <lucas@lucaspickering.me>
2019-08-28 17:13:29 -0400

    favicon.py

--- webapp/favicon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-convert $1 -scale 64x64 favicon-64.png
-convert favicon-64.png -resize 16x16 favicon-16.png
-convert favicon-64.png -resize 32x32 favicon-32.png
-convert favicon-16.png favicon-32.png favicon-64.png ${2-favicon.ico}
-rm favicon-*.png
