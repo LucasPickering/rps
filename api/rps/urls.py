fc6b6179787d88efe1e17b1bb1cc16d34d519f27
Yitao Xiong <yitao@bitsighttech.com>
2019-08-28 17:15:56 -0400

    Added a slack app to contain the logic around slack integration. Right now, starting a new game is supported

--- api/rps/urls.py
+++ api/rps/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
+    path("api/slack/", include("slack.urls")),
     path("api/", include("core.urls")),
 ]
---
+++ api/slack/views.py
@@ -0,0 +1,23 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from core.util import get_livematch_id
+
+
+class SlackIntegrationView(APIView):
+    permission_classes = []  # This API view doesn't need to check CSRF
+
+    def post(self, request, *args, **kwargs):
+        """
+        For now there is only one type of slack integration, which is to generate a new match.
+
+        In the future if we include more workflow like `/rps leaderboard`,
+        we can parse the message here and return different type of responses.
+        """
+        return Response({
+            'response_type': 'in_channel',
+            'text': request.build_absolute_uri('/matches/live/{}'.format(get_livematch_id())),
+            'attachments': [{
+                'text': 'Let the battle begin!'
+            }]
+        })
