b7a0af578561aad03789d07a7f95e6e1bb97d2e6
Yitao Xiong <yitao@bitsighttech.com>
2019-08-29 11:37:37 -0400

    Leaderboard integration with slack (#6)
    * Implemented leaderboard integration with slack

--- api/slack/views.py
+++ api/slack/views.py
@@ -1,11 +1,101 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from core.models import Player
+from core.serializers.players import PlayerSummarySerializer
 from core.util import get_livematch_id
 
 
+def new_game(request, *args, **kwargs):
+    """
+    Return a slack message with a link to a new match
+    """
+    return Response(
+        {
+            "response_type": "in_channel",
+            "text": request.build_absolute_uri(
+                "/matches/live/{}".format(get_livematch_id())
+            ),
+            "attachments": [{"text": "Let the battle begin!"}],
+        }
+    )
+
+
+def leaderboard(request, *args, **kwargs):
+    """
+    Return a slack message with a formatted table containing leaderboard data
+    Slack doesn't support table layout, but it dose support an aweful lot of layouts.
+    There is a chance that this can be better formatted, but as the first release,
+    using .ljust() to make sure each section of strings have the same length.
+    """
+    blocks = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*Leaderboard*"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*Username*         *Wins*     *Losses*     *Win%*"
+            }
+        },
+        {
+            "type": "divider"
+        }
+    ]
+    for player_data in PlayerSummarySerializer(
+            Player.objects.annotate_match_outcomes().order_by('-match_win_pct'),
+            many=True
+    ).data:
+        blocks.append({
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*{username:<23}* "
+                        "{match_win_count:<11} "
+                        "{match_loss_count:<15} "
+                        "{match_win_pct:.3f}".format(
+                            username=player_data.get("username"),
+                            match_win_count=player_data.get("match_win_count"),
+                            match_loss_count=player_data.get("match_loss_count"),
+                            match_win_pct=player_data.get("match_win_pct")
+                        )
+            }
+        })
+    return Response(
+        {
+            "response_type": "in_channel",
+            "blocks": blocks
+        }
+    )
+
+
+def error_response(request, *args, **kwargs):
+    """
+    Return a slack error message
+    """
+    return Response(
+        {
+            "response_type": "in_channel",
+            "text": "Sorry, that operation is not supported. Please try again."
+        }
+    )
+
+
 class SlackIntegrationView(APIView):
-    permission_classes = []  # This API view doesn't need to check CSRF
+    """
+    Slack integration only hits one API endpoint, but with different payloads.
+    Hence implement this view with a bunch of response generators to keep logic organized
+    """
+    permission_classes = []  # This API view doesn"t need to check CSRF
+    RESPONSE_TYPES = {
+        None: new_game,
+        "leaderboard": leaderboard,
+    }
 
     def post(self, request, *args, **kwargs):
         """
@@ -14,12 +104,5 @@ class SlackIntegrationView(APIView):
         In the future if we include more workflow like `/rps leaderboard`,
         we can parse the message here and return different type of responses.
         """
-        return Response(
-            {
-                "response_type": "in_channel",
-                "text": request.build_absolute_uri(
-                    "/matches/live/{}".format(get_livematch_id())
-                ),
-                "attachments": [{"text": "Let the battle begin!"}],
-            }
-        )
+        request_type = request.data.get("text")
+        return self.RESPONSE_TYPES.get(request_type, error_response)(request, *args, **kwargs)
