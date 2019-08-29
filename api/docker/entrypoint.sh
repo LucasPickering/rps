707218808e9224acf46add85b719e9776ab5eed8
Lucas Pickering <lucas@lucaspickering.me>
2019-08-28 21:12:06 -0400

    Automated secret readying in entrypoint

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-if [ ! -z ${RPS_DB_PASSWORD_FILE} ]; then
-    echo "Reading DB password from $RPS_DB_PASSWORD_FILE"
-    export RPS_DB_PASSWORD=$(cat $RPS_DB_PASSWORD_FILE)
-fi
-if [ ! -z ${RPS_SECRET_KEY_FILE} ]; then
-    echo "Reading secret key from $RPS_SECRET_KEY_FILE"
-    export RPS_SECRET_KEY=$(cat $RPS_SECRET_KEY_FILE)
-fi
+# Read each secret file into an env var
+for f in ${SECRETS_DIR-"/run/secrets/"}*; do
+    if [ -e "$f" ]; then
+        # Strip any prefixes before before "rps_", then convert to upper case
+        # GNU sed supports \U to convert to uppercase, but busybox sed doesn't :(
+        var_name=$(echo $f | sed -E 's/^.*?(rps_.*)$/\1/' | tr '[:lower:]' '[:upper:]')
+        echo "Reading \"$f\" into \"$var_name\""
+        export $var_name=$(cat $f) # Load the secret value
+    fi
+done
 exec "$@"
