bef469218edb829dc5e8c2db7f1148f34b53b164
Lucas Pickering <lucas@lucaspickering.me>
2019-09-02 19:27:15 -0400

    Make prunematches work on LivePlayerMatch

--- api/livematch/management/commands/prunematches.py
+++ api/livematch/management/commands/prunematches.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db.models import Q
 from django.utils import timezone
+from itertools import chain
 
-from livematch.models import LiveMatch
+from livematch.models import LiveMatch, LivePlayerMatch
 
 
 logger = logging.getLogger(settings.RPS_LOGGER_NAME)
@@ -25,8 +26,20 @@ class Command(BaseCommand):
     def handle(self, *args, ttl, **options):
         logger.info("Pruning LiveMatch...")
         cutoff_time = timezone.now() - timedelta(seconds=ttl)
-        total, per_model = LiveMatch.objects.filter(
+        matches_to_delete = LiveMatch.objects.filter(
             Q(player1=None) | Q(player1__last_activity__lt=cutoff_time),
             Q(player2=None) | Q(player2__last_activity__lt=cutoff_time),
+            start_time__lt=cutoff_time,
+        )
+        # LivePlayerMatch is a target of an FK on LiveMatch, so they don't
+        # cascade automatically - we have to gather and delete them manually
+        pm_ids_to_delete = set(
+            chain(*matches_to_delete.values_list("player1_id", "player2_id"))
+        )
+        total, per_model = matches_to_delete.delete()
+        pm_total, pm_per_model = LivePlayerMatch.objects.filter(
+            id__in=pm_ids_to_delete
         ).delete()
+        total += pm_total
+        per_model.update(pm_per_model)
         logger.info(f"Deleted {total} rows ({per_model})")
