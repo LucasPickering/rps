b08a8190c811fb361c85bfb072b4fdb88dd23495
Lucas Pickering <lucas@lucaspickering.me>
2019-09-05 22:08:08 -0400

    Match view styling improvements

--- webapp/src/components/matches/live/MoveButtons.tsx
+++ webapp/src/components/matches/live/MoveButtons.tsx
@@ -2,48 +2,16 @@ import { makeStyles, Grid, IconButton } from '@material-ui/core';
 import React, { useContext } from 'react';
 import { Move } from 'state/match';
 import MoveIconCircle from './MoveIconCircle';
-import useScreenSize, { ScreenSize } from 'hooks/useScreenSize';
-import clsx from 'clsx';
 import { LiveMatchContext } from 'state/livematch';
 
 const useLocalStyles = makeStyles(({ spacing }) => ({
-  base: {
-    display: 'flex',
-    '& > button': {
-      margin: spacing(1),
-    },
-  },
-  extended: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(5, 1fr)',
-    gridTemplateRows: 'repeat(3, 1fr)',
-    gridTemplateAreas: `
-    '.. .. ro .. ..'
-    'li .. .. .. pa'
-    '.. sp .. sc ..'
-    `,
-  },
-  small: {
-    gridRowGap: 0,
-  },
-  large: {
-    gridRowGap: 48,
-  },
   button: {
-    padding: 0,
+    padding: spacing(1),
   },
 }));
 
 const baseMoves = [Move.Rock, Move.Paper, Move.Scissors];
-const extendedMoves = baseMoves.concat(Move.Lizard, Move.Spock);
-
-const areaMapping = {
-  [Move.Rock]: 'ro',
-  [Move.Paper]: 'pa',
-  [Move.Scissors]: 'sc',
-  [Move.Lizard]: 'li',
-  [Move.Spock]: 'sp',
-};
+const extendedMoves = [Move.Lizard, Move.Spock];
 
 interface Props {
   disabled: boolean;
@@ -52,33 +20,35 @@ interface Props {
 
 const MoveButtons = ({ disabled, onClick }: Props): React.ReactElement => {
   const localClasses = useLocalStyles();
-  const screenSize = useScreenSize();
   const {
     config: { extendedMode },
   } = useContext(LiveMatchContext);
-  const moves = extendedMode ? extendedMoves : baseMoves;
+
+  const MoveButton: React.FC<{ move: Move }> = ({ move }) => (
+    <IconButton
+      className={localClasses.button}
+      disabled={disabled}
+      onClick={() => onClick(move)}
+    >
+      <MoveIconCircle move={move} />
+    </IconButton>
+  );
 
   return (
-    <Grid
-      className={clsx(
-        extendedMode ? localClasses.extended : localClasses.base,
-        screenSize === ScreenSize.Large
-          ? localClasses.large
-          : localClasses.small
+    <Grid item container justify="center">
+      {/* Break these up into two subcontainers to get the wrapping correct */}
+      <Grid item>
+        {baseMoves.map((move: Move) => (
+          <MoveButton key={move} move={move} />
+        ))}
+      </Grid>
+      {extendedMode && (
+        <Grid item>
+          {extendedMoves.map((move: Move) => (
+            <MoveButton key={move} move={move} />
+          ))}
+        </Grid>
       )}
-      item
-    >
-      {moves.map((move: Move) => (
-        <IconButton
-          key={move}
-          className={localClasses.button}
-          disabled={disabled}
-          style={{ gridArea: areaMapping[move] }}
-          onClick={() => onClick(move)}
-        >
-          <MoveIconCircle move={move} />
-        </IconButton>
-      ))}
     </Grid>
   );
 };
