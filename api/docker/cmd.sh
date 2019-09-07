4302b8eb31779cd28d41fe1c90e403e2d0e76458
Lucas Pickering <lucas@lucaspickering.me>
2019-09-07 15:19:47 -0400

    Use env var for hostname in prod

--- api/docker/cmd.sh
+++ api/docker/cmd.sh
@@ -3,4 +3,4 @@
 crond
 dockerize -wait tcp://${RPS_DB_HOST}:5432
 ./manage.py migrate
-daphne rps.asgi:application -b 0.0.0.0 -p 8000 --access-log ${RPS_LOGGING_DIR}/daphne_access.log
+daphne rps.asgi:application -b 0.0.0.0 -p 8000 --access-log ${RPS_LOGGING_DIR-.}/daphne_access.log
