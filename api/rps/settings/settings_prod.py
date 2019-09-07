001877ac0166c3fe6f4bd49fcf203988d6f58457
Lucas Pickering <lucas@lucaspickering.me>
2019-09-07 15:19:47 -0400

    Use env var for hostname in prod

--- api/rps/settings/settings_prod.py
+++ api/rps/settings/settings_prod.py
@@ -2,7 +2,7 @@ import os
 from .settings import *
 
 
-ALLOWED_HOSTS = ["rps.lucaspickering.me"]
+ALLOWED_HOSTS = [os.environ["RPS_HOSTNAME"]]
 DEBUG = False
 SECRET_KEY = os.environ["RPS_SECRET_KEY"]
 STATIC_URL = "/static/"
