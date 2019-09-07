0c21879de7c18b7c4826f821ba4c95bf29f67a69
Lucas Pickering <lucas@lucaspickering.me>
2019-09-06 23:52:42 -0400

    travis go fuck yourself

--- build_push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-docker-compose -f docker-compose.build.yml build --pull $@
-docker-compose -f docker-compose.build.yml push $@
--- travis_build_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Travis script
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-./build_push.sh api
-./build_push.sh static
-
