5ab15643ae8dbc2ad3c6c5fa26d40745d5a93341
Lucas Pickering <lucas@lucaspickering.me>
2019-09-19 19:22:15 -0400

    Update fixture and fix tests

--- api/core/tests/test_query.py
+++ api/core/tests/test_query.py
@@ -3,8 +3,8 @@ from .test_base import RpsTestCase
 
 class PlayerQuerySetTestCase(RpsTestCase):
     def test_win_pct(self):
-        self.assertAlmostEqual(self.player1.match_win_pct, 0.2, delta=0.001)
-        self.assertAlmostEqual(self.player2.match_win_pct, 0.8, delta=0.001)
+        self.assertAlmostEqual(self.player1.match_win_pct, 0.4, delta=0.001)
+        self.assertAlmostEqual(self.player2.match_win_pct, 0.6, delta=0.001)
 
         self.assertEqual(self.player3.match_count, 0)
         self.assertEqual(self.player3.match_win_pct, 0)
