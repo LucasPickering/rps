ff9851ed92ad3896ee56a2437346ba1babbe8137
Lucas Pickering <lucas@lucaspickering.me>
2019-09-19 19:41:41 -0400

    Hide private live matches from list view

--- api/livematch/views.py
+++ api/livematch/views.py
@@ -4,7 +4,7 @@ from . import models, serializers
 
 
 class LiveMatchesView(generics.ListCreateAPIView):
-    queryset = models.LiveMatch.objects.all()
+    queryset = models.LiveMatch.objects.filters(public=True)
     serializer_class = serializers.LiveMatchSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
