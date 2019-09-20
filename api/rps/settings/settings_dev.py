4c1aaa6e52a939a63deb83721180d9e2a4f5bbaa
Lucas Pickering <lucas@lucaspickering.me>
2019-09-19 23:04:47 -0400

    Set up Django toolbar

--- api/rps/settings/settings_dev.py
+++ api/rps/settings/settings_dev.py
@@ -5,3 +5,4 @@ SECRET_KEY = "ssfzscql8d7rf016q!&16z5@qo@a6_eou-7_76m7gtgs8q^n5j"
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+INTERNAL_IPS = ["172.20.0.2"]
--- api/rps/urls.py
+++ api/rps/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_auth import views as auth_views
+from django.conf import settings
 
 urlpatterns = [
     path(
@@ -22,3 +23,11 @@ urlpatterns = [
         name="password_reset_confirm",
     ),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls))
+    ] + urlpatterns
