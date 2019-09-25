e3c97f20457f8dfa1dfd20aa7d961e06cc0e631f
Lucas Pickering <lucas@lucaspickering.me>
2019-09-24 22:15:04 -0400

    Improve header bar links

---
+++ webapp/src/components/header/HeaderLink.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import clsx from 'clsx';
+import NavLink from 'components/common/NavLink';
+
+const useLocalStyles = makeStyles(({ palette, typography }) => ({
+  link: {
+    height: '100%',
+    backgroundColor: palette.grey[900],
+    color: palette.text.primary,
+    width: 80,
+    ...typography.body1,
+    '&:hover, &:active': {
+      backgroundColor: palette.action.hover,
+      textDecoration: 'none',
+    },
+  },
+  active: {
+    backgroundColor: palette.action.selected,
+    textDecoration: 'none',
+  },
+}));
+
+const HeaderLink: React.FC<React.ComponentProps<typeof NavLink>> = ({
+  className,
+  ...rest
+}) => {
+  // const linkClasses = useLinkStyles();
+  const localClasses = useLocalStyles();
+  return (
+    <NavLink
+      className={clsx(localClasses.link, className)}
+      activeClassName={localClasses.active}
+      {...rest}
+    />
+  );
+};
+
+export default HeaderLink;
