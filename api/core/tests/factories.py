64780d03cb30329142ab4dce0d8b0801e80b20d0
Lucas Pickering <lucas@lucaspickering.me>
2019-10-09 18:26:29 -0400

    Add shitty slack tests

--- api/core/tests/factories.py
+++ api/core/tests/factories.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
+from django.utils import timezone
 from factory import SubFactory
 from factory.django import DjangoModelFactory
 
-from ..models import MatchConfig, Match
+from ..models import MatchConfig, Match, PlayerMatch
 
 
 class UserFactory(DjangoModelFactory):
@@ -27,3 +28,16 @@ class MatchFactory(DjangoModelFactory):
         model = Match
 
     config = SubFactory(MatchConfigFactory)
+
+
+def match_factory(player1, player2, winner):
+    # TODO make this a real factory class
+    m = Match.objects.create(
+        config=MatchConfigFactory(),
+        start_time=timezone.now(),
+        duration=10,
+        winner=winner,
+    )
+    PlayerMatch.objects.create(match=m, player=player1, player_num=0)
+    PlayerMatch.objects.create(match=m, player=player2, player_num=1)
+    return m
