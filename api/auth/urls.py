a92891454898591d5c1fb48179a05f94785bb045
Lucas Pickering <lucas@lucaspickering.me>
2019-10-20 13:14:48 -0400

    Add missing allauth URL rule

--- api/auth/urls.py
+++ api/auth/urls.py
@@ -4,5 +4,10 @@ from .views import GoogleLoginView
 
 urlpatterns = [
     path("", include("rest_auth.urls")),
+    path(
+        "accounts/",
+        include("allauth.socialaccount.urls"),
+        name="socialaccount_signup",
+    ),
     path("google/", GoogleLoginView.as_view()),
 ]
