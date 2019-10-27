53961d0826f5e930f23bf49258aeffb5778b681d
Lucas Pickering <lucas@lucaspickering.me>
2019-10-27 19:53:58 -0400

    Add Manage Account page

--- webapp/src/components/routes/AccountRoutes.tsx
+++ webapp/src/components/routes/AccountRoutes.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginView from 'components/account/LoginView';
-import SocialConnectView from 'components/account/SocialConnectView';
 import NotFoundView from 'components/NotFoundView';
 import GoogleLoginRedirectView from 'components/account/GoogleLoginRedirectView';
 import GoogleConnectRedirectView from 'components/account/GoogleConnectRedirectView';
+import AccountManageView from 'components/account/AccountManageView';
 
 const AccountRoutes: React.FC = () => {
   return (
     <Switch>
       <Route path="/account/login" component={LoginView} exact />
-      <Route path="/account/connect" component={SocialConnectView} exact />
+      <Route path="/account/manage" component={AccountManageView} exact />
 
       <Route
         path="/account/login/redirect/google"
